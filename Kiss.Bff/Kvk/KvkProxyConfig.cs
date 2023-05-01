4d846a479032f0cca92edb4e42b76b450fea9f66
Felix Cornelissen <felix.cornelissen@icatt.nl>
2023-05-01 11:32:56 +0200

    wip

---
+++ Kiss.Bff/Kvk/KvkProxyConfig.cs
@@ -0,0 +1,34 @@
+﻿using Kiss.Bff;
+using Yarp.ReverseProxy.Transforms;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class KvkExtensions
+    {
+        public static IServiceCollection AddKvk(this IServiceCollection services, string baseUrl, string apiKey)
+        {
+            services.AddSingleton<IKissProxyRoute>(new KvkProxyConfig(baseUrl, apiKey));
+            return services;
+        }
+    }
+
+    public sealed class KvkProxyConfig : IKissProxyRoute
+    {
+        private readonly string _apiKey;
+
+        public KvkProxyConfig(string destination, string apiKey)
+        {
+            Destination = destination;
+            _apiKey = apiKey;
+        }
+
+        public string Route => "kvk";
+        public string Destination { get; }
+
+        public ValueTask ApplyRequestTransform(RequestTransformContext context)
+        {
+            context.ProxyRequest.Headers.Add("apikey", _apiKey);
+            return new();
+        }
+    }
+}
