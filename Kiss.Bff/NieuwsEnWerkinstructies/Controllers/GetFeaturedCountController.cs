2e1eb377666e276e692b5c80f4a312650853b0bf
Felix Cornelissen <felix.cornelissen@icatt.nl>
2023-05-08 13:03:21 +0200

    featured count

---
+++ Kiss.Bff/NieuwsEnWerkinstructies/Controllers/GetFeaturedCountController.cs
@@ -0,0 +1,26 @@
+﻿using Kiss.Bff.NieuwsEnWerkinstructies.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kiss.Bff.NieuwsEnWerkinstructies.Controllers
+{
+    [ApiController]
+    public class GetFeaturedCountController : ControllerBase
+    {
+        private readonly NieuwsEnWerkinstructiesDbContext _context;
+
+        public GetFeaturedCountController(NieuwsEnWerkinstructiesDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("api/berichten/featuredcount")]
+        public async Task<IActionResult> GetCount(CancellationToken token)
+        {
+            var count = await _context.Berichten.Where(x => x.IsBelangrijk).CountAsync(token);
+            return Ok(new FeaturedCount(count));
+        }
+    }
+
+    public record struct FeaturedCount(int Count);
+}
