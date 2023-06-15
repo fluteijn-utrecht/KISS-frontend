60916965e75603791a9da4e56282848e4738fc20
Felix Cornelissen <felix.cornelissen@icatt.nl>
2023-06-15 18:21:06 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,6 @@ import { SwitchableStoreProvider } from "./stores/switchable-store";
 
 const contactmomentStore = useContactmomentStore();
 const route = useRoute();
-// test lint
 </script>
 
 <style lang="scss">
