ac94652efb4c91530a21d3ba9b06bf3ee45c7cef
Felix Cornelissen <felix.cornelissen@icatt.nl>
2023-06-19 17:45:14 +0200

    fix nieuwsbericht koppelen

--- src/views/AfhandelingView.vue
+++ src/views/AfhandelingView.vue
@@ -685,7 +685,13 @@ const addNieuwsberichtToContactmoment = (
   vraag.nieuwsberichten.forEach((nieuwsbericht) => {
     if (!nieuwsbericht.shouldStore) return;
 
-    contactmoment.onderwerpLinks.push(nieuwsbericht.nieuwsbericht.url);
+    //make absolute if not already
+    const absoluteUrl = new URL(
+      nieuwsbericht.nieuwsbericht.url,
+      window.location.origin
+    );
+
+    contactmoment.onderwerpLinks.push(absoluteUrl.toString());
   });
 };
 
@@ -698,7 +704,7 @@ const addWerkinstructiesToContactmoment = (
   vraag.werkinstructies.forEach((werkinstructie) => {
     if (!werkinstructie.shouldStore) return;
 
-    //make absolute if not already     
+    //make absolute if not already
     const absoluteUrl = new URL(
       werkinstructie.werkinstructie.url,
       window.location.origin
