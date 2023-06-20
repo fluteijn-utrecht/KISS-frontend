d657f894bd3ae35aae64769bd2c832ca8fde46c8
mark stoker <mark@icatt.nl>
2023-06-20 09:27:08 +0200

    setup

---
+++ Kiss.Bff/Beheer/Management/GetContactmomentManagementLog.cs
@@ -0,0 +1,32 @@
+﻿using Kiss.Bff.Beheer.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kiss.Bff.Beheer.Management
+{
+
+
+    [ApiController]
+    public class GetContactmomentManagementLog : ControllerBase
+    {
+        private readonly BeheerDbContext _db;
+
+        public GetContactmomentManagementLog(BeheerDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("/api/management/contactmomenten")]
+        public IActionResult Get()
+        {
+
+            var contactmomenten =  _db.ContactmomentManagementLogs
+                .OrderByDescending(x => x.Startdatum)
+                .Select(x => new { x.EmailadresKcm })
+                .Take(10000)
+                .AsAsyncEnumerable();
+
+            return Ok(contactmomenten);
+        }
+    }
+}
