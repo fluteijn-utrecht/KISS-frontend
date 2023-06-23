5eb058421018fe3b6f45210bc818b88a2b8e86ac
mark stoker <mark@icatt.nl>
2023-06-23 14:08:25 +0200

    formsubmittertest wip

---
+++ src/test/services/service-data/fromSubmitter.test.ts
@@ -0,0 +1,82 @@
+import { flushPromises, mount } from "@vue/test-utils";
+import {
+  ServiceResult,
+  fetchLoggedIn,
+  mapServiceData,
+  parseJson,
+  throwIfNotOk,
+  type ServiceData,
+} from "@/services";
+
+import {
+  assertType,
+  describe,
+  expect,
+  expectTypeOf,
+  test,
+  type Test,
+} from "vitest";
+
+describe("service-data-test", () => {
+  test("ServiceResult.fromSubmitter returns success for a resolved Promise.", async () => {
+    type TestType = {
+      data: string;
+    };
+
+    const fromSubmitterResult = ServiceResult.fromSubmitter<TestType, TestType>(
+      () => Promise.resolve({ data: "out" } as TestType)
+    );
+
+    fromSubmitterResult.submit({ data: "in" });
+
+    expectTypeOf(fromSubmitterResult).toMatchTypeOf<ServiceData<TestType>>();
+
+    await flushPromises();
+
+    expect(fromSubmitterResult.state).toMatch("success");
+    expect(fromSubmitterResult.success).toBeTruthy();
+    if (fromSubmitterResult.success) {
+      expect((fromSubmitterResult.data as TestType).data).toMatch("out");
+    }
+  });
+
+  test("ServiceResult.fromSubmitter returns a loading state for a pending Promise.", async () => {
+    type TestType = {
+      data: string;
+    };
+
+    const fromPromiseResult = ServiceResult.fromSubmitter<TestType, TestType>(
+      () => Promise.resolve({ data: "value" } as TestType)
+    );
+
+    fromPromiseResult.submit({ data: "in" });
+
+    expectTypeOf(fromPromiseResult).toMatchTypeOf<ServiceData<TestType>>();
+
+    expect(fromPromiseResult.state).toMatch("loading");
+    expect(fromPromiseResult.success).toBeFalsy();
+  });
+
+  test("ServiceResult.fromSubmitter returns an eroor state for a rejected Promise.", async () => {
+    type TestType = {
+      data: string;
+    };
+
+    // let x;
+    const fromPromiseResult = ServiceResult.fromSubmitter<TestType, TestType>(
+      () => Promise.reject<TestType>()
+    );
+
+    fromPromiseResult.submit({ data: "in" });
+
+    await flushPromises();
+
+    //expect(y.state).toMatch("loading");
+    //expect(y.success).toBeFalsy();
+
+    //  x.catch(() => {
+    expect(fromPromiseResult.state).toMatch("error");
+    expect(fromPromiseResult.success).toBeFalsy();
+    //   });
+  });
+});
