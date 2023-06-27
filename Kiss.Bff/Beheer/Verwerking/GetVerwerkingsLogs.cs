05647243d7db146b2e91a7062a6284559a5475f4
Felix Cornelissen <felix.cornelissen@icatt.nl>
2023-06-27 11:22:30 +0200

    ADD endpoint to get verwerkingslogs

---
+++ Kiss.Bff/Beheer/Verwerking/GetVerwerkingsLogs.cs
@@ -0,0 +1,28 @@
+﻿using Kiss.Bff.Beheer.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kiss.Bff.Beheer.Verwerking
+{
+    [ApiController]
+    public class GetVerwerkingsLogs : ControllerBase
+    {
+        private readonly BeheerDbContext _db;
+
+        public GetVerwerkingsLogs(BeheerDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("api/verwerkingslogs")]
+        public IActionResult Get()
+        {
+            var data = _db.VerwerkingsLogs
+                .OrderByDescending(x => x.InsertedAt)
+                .Take(10000)
+                .AsAsyncEnumerable();
+
+            return Ok(data);
+        }
+    }
+}
