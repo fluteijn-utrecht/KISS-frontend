f269e1b4bf707b743f77b45754cee0f9007b6ca1
Felix Cornelissen <felix.cornelissen@icatt.nl>
2023-07-05 14:34:09 +0200

    FIX: application not starting because of public method in SendFeedback.cs

--- Kiss.Bff.Test/SendFeedbackTest.cs
+++ Kiss.Bff.Test/SendFeedbackTest.cs
@@ -1,52 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using IdentityModel;
-using Kiss.Bff.Feedback;
-using Kiss.Bff.NieuwsEnWerkinstructies.Controllers;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Moq;
+﻿using Kiss.Bff.Feedback;
 
 namespace Kiss.Bff.Test
 {
     [TestClass]
     public class SendFeedbackTest : TestHelper
     {
-
-        private SendFeedback _controller;
-        private Mock<SmtpClient> _smtpClientMock;
-        private Mock<IConfiguration> _configurationMock;
-
-        [TestInitialize]
-        public void Initialize()
-        {
-            _smtpClientMock = new Mock<SmtpClient>();
-            _configurationMock = new Mock<IConfiguration>();
-
-            _controller = new SendFeedback(_smtpClientMock.Object, _configurationMock.Object);
-
-            // Mock the User for the controller
-            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
-            {
-                new Claim(JwtClaimTypes.Name, "John Doe"),
-                new Claim(JwtClaimTypes.Email, "john.doe@example.com"),
-            }, "mock"));
-
-            _controller.ControllerContext = new ControllerContext
-            {
-                HttpContext = new DefaultHttpContext
-                {
-                    User = user
-                }
-            };
-        }
-
         [TestMethod]
         public void BuildEmailBody_ContainsUserName()
         {
@@ -55,7 +13,7 @@ namespace Kiss.Bff.Test
             var userName = "John Doe";
 
             // Act
-            var result = _controller.BuildEmailBody(userName, model);
+            var result = SendFeedback.BuildEmailBody(userName, model);
 
             // Assert
             Assert.IsTrue(result.Contains(userName));
