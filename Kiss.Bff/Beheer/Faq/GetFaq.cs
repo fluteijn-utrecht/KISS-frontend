474f009f6a260607f7f2a72a606ae62b7ce56b6a
Felix Cornelissen <felix.cornelissen@icatt.nl>
2023-07-11 17:10:05 +0200

    wip

---
+++ Kiss.Bff/Beheer/Faq/GetFaq.cs
@@ -0,0 +1,45 @@
+﻿using Kiss.Bff.Beheer.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kiss.Bff.Beheer.Faq
+{
+    [ApiController]
+    [Route("api/faq")]
+    public class GetFaq: ControllerBase
+    {
+        private const int AmountOfContactmomenten = 500;
+        private const int AmountOfQuestions = 10;
+
+        private readonly BeheerDbContext _db;
+
+        public GetFaq(BeheerDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var lastContactmomenten = _db.ContactmomentManagementLogs
+                .OrderByDescending(x => x.Einddatum)
+                .Take(AmountOfContactmomenten);
+
+            var groupedByQuestion = lastContactmomenten
+                .Where(x => !string.IsNullOrWhiteSpace(x.PrimaireVraagWeergave) || !string.IsNullOrWhiteSpace(x.AfwijkendOnderwerp))
+                .GroupBy(x => string.IsNullOrWhiteSpace(x.PrimaireVraagWeergave) 
+                    ? x.AfwijkendOnderwerp 
+                    : x.PrimaireVraagWeergave);
+
+            var topQuestions = groupedByQuestion
+                .OrderByDescending(x => x.Count())
+                .Take(AmountOfQuestions);
+
+            var query = topQuestions
+                .Select(x => x.Key)
+                .AsAsyncEnumerable();
+
+            return Ok(query);
+        }
+    }
+}
