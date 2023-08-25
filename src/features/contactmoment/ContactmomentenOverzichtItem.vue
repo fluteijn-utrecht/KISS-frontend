240e20ea76c30763e81fef1c06be601587c5cf8e
mark stoker <mark@icatt.nl>
2023-08-25 12:31:15 +0200

    cleanup

--- src/features/contactmoment/ContactmomentenOverzichtItem.vue
+++ src/features/contactmoment/ContactmomentenOverzichtItem.vue
@@ -1,49 +1,47 @@
 <template>
-
-    <summary>
-      <span aria-describedby="datum-header" class="first-column">
-        <DutchDate v-if="registratieDatum" :date="registratieDatum" />
-      </span>
-      <span aria-describedby="medewerker-header">{{
-        fullName(contactmoment.medewerkerIdentificatie)
-      }}</span>
-      <span aria-describedby="kanaal-header">{{ contactmoment.kanaal }}</span>
-      <span aria-describedby="gespreksresultaat-header">
-        <ServiceDataWrapper :data="cmDetails">
-          <template #success="{ data }">
-            {{ data?.gespreksresultaat }}
-          </template>
-        </ServiceDataWrapper>
-      </span>
-    </summary>
-    <dl>
-      <dt>Starttijd</dt>
-      <dd>
-        <DutchTime v-if="registratieDatum" :date="registratieDatum" />
-      </dd>
-      <slot
-        name="object"
-        v-for="object in contactmoment.objectcontactmomenten"
-        :key="object.object"
-        :object="object"
-      />
+  <summary>
+    <span aria-describedby="datum-header" class="first-column">
+      <DutchDate v-if="registratieDatum" :date="registratieDatum" />
+    </span>
+    <span aria-describedby="medewerker-header">{{
+      fullName(contactmoment.medewerkerIdentificatie)
+    }}</span>
+    <span aria-describedby="kanaal-header">{{ contactmoment.kanaal }}</span>
+    <span aria-describedby="gespreksresultaat-header">
       <ServiceDataWrapper :data="cmDetails">
-        <template #success="{ data: contactmoment }">
-          <template v-if="contactmoment?.vraag">
-            <dt>Vraag</dt>
-            <dd class="tekst">{{ contactmoment.vraag }}</dd>
-          </template>
-          <template v-if="contactmoment?.specifiekeVraag">
-            <dt>Specifieke vraag</dt>
-            <dd class="tekst">{{ contactmoment.specifiekeVraag }}</dd>
-          </template>
+        <template #success="{ data }">
+          {{ data?.gespreksresultaat }}
         </template>
       </ServiceDataWrapper>
+    </span>
+  </summary>
+  <dl>
+    <dt>Starttijd</dt>
+    <dd>
+      <DutchTime v-if="registratieDatum" :date="registratieDatum" />
+    </dd>
+    <slot
+      name="object"
+      v-for="object in contactmoment.objectcontactmomenten"
+      :key="object.object"
+      :object="object"
+    />
+    <ServiceDataWrapper :data="cmDetails">
+      <template #success="{ data: contactmoment }">
+        <template v-if="contactmoment?.vraag">
+          <dt>Vraag</dt>
+          <dd class="tekst">{{ contactmoment.vraag }}</dd>
+        </template>
+        <template v-if="contactmoment?.specifiekeVraag">
+          <dt>Specifieke vraag</dt>
+          <dd class="tekst">{{ contactmoment.specifiekeVraag }}</dd>
+        </template>
+      </template>
+    </ServiceDataWrapper>
 
-      <dt>Notitie</dt>
-      <dd class="tekst">{{ contactmoment.tekst }}</dd>
-    </dl>
-
+    <dt>Notitie</dt>
+    <dd class="tekst">{{ contactmoment.tekst }}</dd>
+  </dl>
 </template>
 
 <script setup lang="ts">
@@ -62,11 +60,5 @@ const registratieDatum = computed(
     new Date(props.contactmoment.registratiedatum)
 );
 
-
-
 const cmDetails = useContactmomentDetails(() => props.contactmoment.url);
 </script>
-
-<style lang="scss" scoped>
-
-</style>
