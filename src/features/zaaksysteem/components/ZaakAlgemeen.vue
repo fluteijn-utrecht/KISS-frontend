27f6031cf78e9c9efda98f67932da170b64d3801
Felix Cornelissen <felix.cornelissen@icatt.nl>
2023-08-30 09:54:56 +0200

    zaak algemeen tab columns 2

--- src/features/zaaksysteem/components/ZaakAlgemeen.vue
+++ src/features/zaaksysteem/components/ZaakAlgemeen.vue
@@ -1,45 +1,25 @@
 <template>
   <utrecht-heading :level="2" modelValue>Algemeen</utrecht-heading>
-  <div>
-    <dl>
-      <dt>Titel</dt>
-      <dd>{{ zaak.omschrijving }}</dd>
-      <dt>Zaaktype</dt>
-      <dd>{{ zaak.zaaktypeOmschrijving }}</dd>
-      <dt>Zaaknummer</dt>
-      <dd>{{ zaak.identificatie }}</dd>
-      <dt>Indiendatum</dt>
-      <dd>
-        <time-or-onbekend class="value" :date="zaak.startdatum" />
-      </dd>
-    </dl>
-    <dl>
-      <dt>Aanvrager</dt>
-      <dd>{{ zaak.aanvrager }}</dd>
-      <dt>Status</dt>
-      <dd>{{ zaak.status }}</dd>
-      <dt>Behandelaar</dt>
-      <dd>{{ zaak.behandelaar }}</dd>
-      <!-- <dt>Registratiedatum</dt>
-      <dd>
-        <time-or-onbekend class="value" :date="zaak.registratieDatum" />
-      </dd>
-      <dt>Startdatum</dt>
-      <dd>
-        <time-or-onbekend class="value" :date="zaak.startdatum" />
-      </dd>
-      <dt>Streefdatum</dt>
-      <dd>
-        <time-or-onbekend class="value" :date="zaak.streefDatum" />
-      </dd>
-      <dt>Fatale datum</dt>
-      <dd>
-        <time-or-onbekend class="value" :date="zaak.fataleDatum" />
-      </dd> -->
-      <dt>Toelichting</dt>
-      <dd>{{ zaak.toelichting }}</dd>
-    </dl>
-  </div>
+  <dl>
+    <dt>Titel</dt>
+    <dd>{{ zaak.omschrijving }}</dd>
+    <dt>Zaaktype</dt>
+    <dd>{{ zaak.zaaktypeOmschrijving }}</dd>
+    <dt>Zaaknummer</dt>
+    <dd>{{ zaak.identificatie }}</dd>
+    <dt>Indiendatum</dt>
+    <dd>
+      <time-or-onbekend class="value" :date="zaak.startdatum" />
+    </dd>
+    <dt>Aanvrager</dt>
+    <dd>{{ zaak.aanvrager }}</dd>
+    <dt>Status</dt>
+    <dd>{{ zaak.status }}</dd>
+    <dt>Behandelaar</dt>
+    <dd>{{ zaak.behandelaar }}</dd>
+    <dt>Toelichting</dt>
+    <dd>{{ zaak.toelichting }}</dd>
+  </dl>
 </template>
 
 <script setup lang="ts">
@@ -53,18 +33,16 @@ defineProps<{
 </script>
 
 <style scoped lang="scss">
-div {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 8rem;
-  margin-block-start: var(--spacing-default);
+dl {
+  margin-block-start: var(--line-height-default);
+  columns: 2;
 }
 
-dl {
-  block-size: max-content;
-  display: grid;
-  grid-template-columns: auto auto;
-  column-gap: var(--spacing-large);
-  row-gap: var(--line-height-default);
+dd {
+  break-before: avoid;
+}
+
+dt:not(:first-of-type) {
+  margin-block-start: var(--line-height-default);
 }
 </style>
