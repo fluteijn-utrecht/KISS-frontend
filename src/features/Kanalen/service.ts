737efb892a844238c3fea6bd18ebd15ee75df20e
mark stoker <mark@icatt.nl>
2024-04-02 12:54:04 +0200

    cleanup

--- src/features/Kanalen/service.ts
+++ src/features/Kanalen/service.ts
@@ -2,16 +2,22 @@ import { fetchLoggedIn, ServiceResult, throwIfNotOk } from "@/services";
 
 async function fetchAll(url: string) {
   return await fetchLoggedIn(url)
-  .then(throwIfNotOk)
-  .then((r) => r.json());
+    .then(throwIfNotOk)
+    .then((r) => r.json());
 }
 
 export function useKanalen() {
-  const kanalen = ServiceResult.fromFetcher("/api/KanalenBeheerOverzicht", fetchAll, { poll: false } );
+  const kanalen = ServiceResult.fromFetcher(
+    "/api/KanalenBeheerOverzicht",
+    fetchAll,
+  );
 
-  return kanalen
+  return kanalen;
 }
 
 export function useKanalenKeuzeLijst() {
-  return ServiceResult.fromFetcher("/api/KanalenContactmomentKeuzelijst", fetchAll);
+  return ServiceResult.fromFetcher(
+    "/api/KanalenContactmomentKeuzelijst",
+    fetchAll,
+  );
 }
