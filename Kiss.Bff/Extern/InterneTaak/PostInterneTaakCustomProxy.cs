a11d50b6eec044305f65ccb9d1cc47040319777a
githubjimmeicatt <jimme@icatt.nl>
2024-05-14 15:35:08 +0200

    configurabel maken van variable

--- Kiss.Bff/Extern/InterneTaak/PostInterneTaakCustomProxy.cs
+++ Kiss.Bff/Extern/InterneTaak/PostInterneTaakCustomProxy.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Kiss.Bff.InterneTaak
 {
     [ApiController]
+    [Route("api/internetaak/api/{version}/objects")]
     public class PostInterneTaakCustomProxy : ControllerBase
     {
         private readonly InterneTaakProxyConfig _config;
@@ -15,20 +16,22 @@ namespace Kiss.Bff.InterneTaak
             _getMedewerkerIdentificatie = getMedewerkerIdentificatie;
         }
 
-        [HttpPost("api/internetaak/api/{version}/objects")]
+        [HttpPost]
         public IActionResult Post([FromRoute] string version, [FromBody] JsonObject node)
         {
             node["type"] = _config.ObjectTypeUrl;
 
-            if (node.TryGetPropertyValue("record", out var record)
-                && record is JsonObject recordObj
-                && recordObj.TryGetPropertyValue("data", out var data)
-                && data is JsonObject dataObj)
+            if (node.TryGetPropertyValue("record", out var record) && record is JsonObject recordObj)
             {
-                dataObj["medewerkerIdentificatie"] = _getMedewerkerIdentificatie();
+                recordObj["typeVersion"] = _config.TypeVersion;
+
+                if (recordObj.TryGetPropertyValue("data", out var data) && data is JsonObject dataObj)
+                {
+                    dataObj["medewerkerIdentificatie"] = _getMedewerkerIdentificatie();
+                }
             }
 
-            var url = $"{_config.Destination.AsSpan().TrimEnd('/')}/api/{version}/objects";
+            var url = $"{_config.Destination.TrimEnd('/')}/api/{version}/objects";
 
             return new ProxyResult(() =>
             {
