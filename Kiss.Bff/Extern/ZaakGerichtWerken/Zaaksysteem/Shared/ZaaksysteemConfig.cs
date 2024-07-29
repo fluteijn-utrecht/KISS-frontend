c303ce9c85cd23c54276dc3c58678a441f3e1fab
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-07-29 10:41:05 +0200

    feat: meerdere zaaksystemen

---
+++ Kiss.Bff/Extern/ZaakGerichtWerken/Zaaksysteem/Shared/ZaaksysteemConfig.cs
@@ -0,0 +1,4 @@
+﻿namespace Kiss.Bff.Extern.ZaakGerichtWerken.Zaaksysteem.Shared
+{
+    public record ZaaksysteemConfig(string BaseUrl, string ClientId, string Secret, string? DeeplinkBaseUrl, string? DeeplinkProperty);
+}
