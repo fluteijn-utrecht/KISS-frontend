ed76d2d718786ea82a18971356cc5ec7482da6fb
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-10-31 16:03:06 +0100

    cleanup

--- src/services/use-async.ts
+++ src/services/use-async.ts
@@ -1,14 +1,18 @@
 import { asyncComputed } from "@vueuse/core";
 import { ref } from "vue";
 
-export const useAsync = <T>(handler: () => Promise<T> | undefined) => {
+export const useAsync = <T>(
+  handler: (signal: AbortSignal) => Promise<T> | undefined,
+) => {
   const loading = ref(false);
   const error = ref(false);
 
   const data = asyncComputed<T | undefined>(
-    () => {
+    (onCancel) => {
+      const controller = new AbortController();
+      onCancel(() => controller.abort());
       error.value = false;
-      return handler();
+      return handler(controller.signal);
     },
     undefined,
     {
