a19c96cccc8af7f69d70a50bdece198fe6da12b1
nijmra <armijn@gmail.com>
2024-11-05 11:22:10 +0100

    fix height aside

--- src/layout/TheHeader.vue
+++ src/layout/TheHeader.vue
@@ -123,9 +123,11 @@ header {
     "nav nav";
   grid-template-columns: auto 6rem;
   align-items: center;
+  min-height: var(--header-min-height);
 
   &:has(.search-results) {
     background-color: var(--color-secondary);
+    min-height: auto;
   }
 
   .log-out {
