615ed15c53752d99f6409494411ba2f273923e31
githubjimmeicatt <jimme@icatt.nl>
2024-11-07 17:29:32 +0100

    no message

--- Kiss.Bff/Extern/ZaakGerichtWerken/Contactmomenten/ContactmomentDetailsRapportageOverzicht.cs
+++ Kiss.Bff/Extern/ZaakGerichtWerken/Contactmomenten/ContactmomentDetailsRapportageOverzicht.cs
@@ -3,10 +3,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 
 namespace Kiss.Bff.Extern.ZaakGerichtWerken.Contactmomenten
 {
