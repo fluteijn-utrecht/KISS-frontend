2b3e0e0b4f8deb5deb1adab6d9209cc9c93b191d
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-11-07 16:34:30 +0100

    fix test

--- Kiss.Bff/Intern/Data/Migrations/20241106133641_AddContactMomentDetailsBronnen.cs
+++ Kiss.Bff/Intern/Data/Migrations/20241106133641_AddContactMomentDetailsBronnen.cs
@@ -9,9 +9,6 @@ namespace Kiss.Bff.NieuwsEnWerkinstructies.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Bronnen");
-
             migrationBuilder.CreateTable(
                 name: "ContactMomentDetailsBronnen",
                 columns: table => new
@@ -44,33 +41,6 @@ namespace Kiss.Bff.NieuwsEnWerkinstructies.Migrations
         {
             migrationBuilder.DropTable(
                 name: "ContactMomentDetailsBronnen");
-
-            migrationBuilder.CreateTable(
-                name: "Bronnen",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    ContactmomentDetailsId = table.Column<string>(type: "text", nullable: false),
-                    Soort = table.Column<string>(type: "text", nullable: false),
-                    Titel = table.Column<string>(type: "text", nullable: false),
-                    Url = table.Column<string>(type: "text", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Bronnen", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Bronnen_ContactMomentDetails_ContactmomentDetailsId",
-                        column: x => x.ContactmomentDetailsId,
-                        principalTable: "ContactMomentDetails",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Bronnen_ContactmomentDetailsId",
-                table: "Bronnen",
-                column: "ContactmomentDetailsId");
         }
     }
 }
