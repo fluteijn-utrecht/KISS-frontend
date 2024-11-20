c46901b9d52b642f9e4a8572e9b3fc80ac95bd5a
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-11-20 11:04:19 +0100

    cleanup types

--- src/stores/create-store.ts
+++ src/stores/create-store.ts
@@ -1,11 +1,10 @@
-import {
-  inject,
-  provide,
-  ref,
-  type InjectionKey,
-  type Ref,
-  type UnwrapRef,
-} from "vue";
+import { inject, provide, ref, type InjectionKey } from "vue";
+
+// het type dat de ref functie van vue retourneert is ingewikkeld en kan per versie verschillen
+// op deze manier kunnen we het type 'uitrekenen' en verandert het mee met versie updates van vue
+const refWithValueSpecified = <T>(v: T) => ref(v);
+type RefReturnType<T> = ReturnType<typeof refWithValueSpecified<T>>;
+type RefValue<T> = RefReturnType<T>["value"];
 
 const injectionKey = Symbol() as InjectionKey<StoreImplementation>;
 const storeMap = new Map<string, Store<unknown>>();
@@ -13,11 +12,11 @@ const storeMap = new Map<string, Store<unknown>>();
 export type CreateStoreParams<T> = {
   stateId: string;
   stateFactory: () => T;
-  onExistingState?: (state: UnwrapRef<T>) => void | Promise<void>;
-  onNewState?: (state: UnwrapRef<T>) => void | Promise<void>;
+  onExistingState?: (state: RefValue<T>) => void | Promise<void>;
+  onNewState?: (state: RefValue<T>) => void | Promise<void>;
 };
 
-export type Store<T> = Ref<UnwrapRef<T>> & {
+export type Store<T> = RefReturnType<T> & {
   reset: () => void;
 };
 
@@ -32,11 +31,13 @@ export function defaultStoreImplementation<T>({
   let store = storeMap.get(stateId) as Store<T> | undefined;
 
   if (!store) {
-    store = Object.assign(ref(stateFactory()) as Ref<UnwrapRef<T>>, {
+    store = Object.assign(ref(stateFactory()), {
       reset() {
-        store!.value = stateFactory() as UnwrapRef<T>;
+        if (store) {
+          store.value = stateFactory();
+        }
       },
-    }) as Store<T>;
+    });
 
     storeMap.set(stateId, store);
     onNewState?.(store.value);
@@ -55,7 +56,7 @@ export function provideStoreImplementation(
 
 export function ensureState<T>(params: CreateStoreParams<T>): Store<T> {
   const implementation = inject(injectionKey, defaultStoreImplementation);
-  return implementation(params) as Store<T>;
+  return implementation(params);
 }
 
 export function resetState(stateId: string) {
