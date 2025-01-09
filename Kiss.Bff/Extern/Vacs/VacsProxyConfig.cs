2a748b467f95e06b63a6fd3df0c781f916c586ff
nijmra <armijn@gmail.com>
2025-01-09 12:11:34 +0100

    backend autorisatie

--- Kiss.Bff/Extern/Vacs/VacsProxyConfig.cs
+++ Kiss.Bff/Extern/Vacs/VacsProxyConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Authorization;
 using Yarp.ReverseProxy.Transforms;
 
 namespace Kiss.Bff.Vacs
@@ -6,17 +7,32 @@ namespace Kiss.Bff.Vacs
     public static class VacsExtensions
     {
         public static IServiceCollection AddVacsProxy(this IServiceCollection services, string destination, string token, string objectTypeUrl, string typeVersion)
-            => services.AddSingleton(new VacsProxyConfig(destination, token, objectTypeUrl, typeVersion))
-            .AddSingleton<IKissProxyRoute>(s => s.GetRequiredService<VacsProxyConfig>());
+        {
+            return services.AddSingleton<IKissProxyRoute>(s =>
+            {
+                var authorizationService = s.GetRequiredService<IAuthorizationService>();
+                var policyProvider = s.GetRequiredService<IAuthorizationPolicyProvider>();
+
+                return new VacsProxyConfig(destination, token, objectTypeUrl, typeVersion, authorizationService, policyProvider);
+            });
+        }
     }
 
     public class VacsProxyConfig : IKissProxyRoute
     {
-        public VacsProxyConfig(string destination, string token, string objectTypeUrl, string typeVersion)
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IAuthorizationPolicyProvider _policyProvider;
+        private readonly string _token;
+
+        public VacsProxyConfig(string destination, string token, string objectTypeUrl, string typeVersion,
+            IAuthorizationService authorizationService,
+            IAuthorizationPolicyProvider policyProvider)
         {
             Destination = destination;
             ObjectTypeUrl = objectTypeUrl;
             TypeVersion = typeVersion ?? "1";
+            _authorizationService = authorizationService;
+            _policyProvider = policyProvider;
             _token = token;
         }
 
@@ -26,21 +42,33 @@ namespace Kiss.Bff.Vacs
         public string ObjectTypeUrl { get; }
         public string TypeVersion { get; }
 
-        private readonly string _token;
-
-        public ValueTask ApplyRequestTransform(RequestTransformContext context)
+        public async ValueTask ApplyRequestTransform(RequestTransformContext context)
         {
-            ApplyHeaders(context.ProxyRequest.Headers, context.HttpContext.User);
+            var policy = await _policyProvider.GetPolicyAsync(Policies.RedactiePolicy);
+            if (policy == null)
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                return;
+            }
+            
+            var authResult = await _authorizationService.AuthorizeAsync(context.HttpContext.User, null, policy);
+            if (!authResult.Succeeded)
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                return;
+            }
+
+            ApplyHeaders(context.ProxyRequest.Headers);
+
             var request = context.HttpContext.Request;
             var isObjectsEndpoint = request.Path.Value?.AsSpan().TrimEnd('/').EndsWith("objects") ?? false;
             if (request.Method == HttpMethods.Get && isObjectsEndpoint)
             {
                 context.Query.Collection["type"] = new(ObjectTypeUrl);
             }
-            return new();
         }
 
-        public void ApplyHeaders(HttpRequestHeaders headers, System.Security.Claims.ClaimsPrincipal user)
+        public void ApplyHeaders(HttpRequestHeaders headers)
         {
             headers.Authorization = new AuthenticationHeaderValue("Token", _token);
             headers.Add("Content-Crs", "EPSG:4326");
