755f79bba24f843787f4582d329322a6ddaab973
Henok Solomon <henok@icatt.nl>
2025-01-22 17:31:31 +0300

    Add test scenarios and locators for ContactMomentSearch
    Added necessary using directives in ContactMomentSearchScenarios.cs and Locators.cs. Introduced a new namespace Kiss.Bff.EndToEndTest.ContactMomentSearch.Helpers in Locators.cs. Created ContactMomentSearchScenarios class inheriting from KissPlaywrightTest with two placeholder test methods: SearchByLastNameAndDOB_ValidAsync and SearchByLastNameAndDOB_NotFound. Added Locators static class with methods to retrieve various input fields and elements on a page.

---
+++ Kiss.Bff.EndToEndTest/ContactMomentSearch/ContactMomentSearchScenarios.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kiss.Bff.EndToEndTest.ContactMomentSearch.Helpers;
+
+namespace Kiss.Bff.EndToEndTest.ContactMomentSearch
+{
+    [TestClass]
+    public class ContactMomentSearchScenarios : KissPlaywrightTest
+    {
+        #region Test Cases
+
+        // 1. Searching by Last Name and Date of Birth (Valid)
+        [TestMethod]
+        public async Task SearchByLastNameAndDOB_ValidAsync()
+        {
+
+
+        }
+
+        // 2. Searching by Last Name and Date of Birth (Not Found)
+        [TestMethod]
+        public async Task SearchByLastNameAndDOB_NotFound()
+        { 
+            
+        }
+
+        #endregion
+    }
+}
