84d321469f687efcc4b409a11b5840620c7d2310
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-01-23 11:34:37 +0100

    upgrade linting packages

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import pluginVue from "eslint-plugin-vue";
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from "@vue/eslint-config-typescript";
+import pluginVitest from "@vitest/eslint-plugin";
+import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
+import js from "@eslint/js";
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  {
+    name: "app/files-to-lint",
+    files: ["**/*.{ts,mts,tsx,vue}"],
+  },
+
+  {
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
+  },
+  js.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  vueTsConfigs.recommended,
+  {
+    ...pluginVitest.configs.recommended,
+    files: ["src/**/__tests__/*"],
+  },
+  skipFormatting,
+  {
+    rules: {
+      "vue/multi-word-component-names": [
+        "error",
+        {
+          ignores: ["Pagination", "Paragraph"],
+        },
+      ],
+      // TODO: treat these rules as errors and fix all affected code
+      "vue/no-setup-props-destructure": ["warn"],
+      "vue/prefer-import-from-vue": ["warn"],
+    },
+  },
+);
