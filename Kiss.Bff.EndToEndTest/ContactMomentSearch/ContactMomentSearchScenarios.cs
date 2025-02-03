4ba101586690f494e914455034f2e4d1c2f611a3
Henok Solomon <henok@icatt.nl>
2025-02-03 20:23:10 +0300

    Update test methods in ContactMomentSearchScenarios.cs
    Updated SearchByLastNameAndDOB_ValidAsync and SearchByLastNameAndDOB_NotFoundAsync methods:
    - Added steps to navigate to the homepage and start a new contact moment.
    - Updated descriptions for filling in last name and birth date fields.
    - Added steps to click the search button.
    - Updated descriptions for verifying navigation and "no results found" message.

--- Kiss.Bff.EndToEndTest/ContactMomentSearch/ContactMomentSearchScenarios.cs
+++ Kiss.Bff.EndToEndTest/ContactMomentSearch/ContactMomentSearchScenarios.cs
@@ -17,15 +17,22 @@ namespace Kiss.Bff.EndToEndTest.ContactMomentSearch
         {
             await Step("Given the user is on the startpagina");
 
+            await Page.GotoAsync("/");
+
+            await Step("When user starts a new contactmoment");
+
             await Page.CreateNewContactmomentAsync();
 
-            await Step("User fills Lastname and Birthday and perform search");
+            await Step("And user enters \"Burck\" in the field achternaam and enters \"17-11-1952\" in the field geboortedatum ");
 
             await Page.Personen_LastNameInput().FillAsync("Burck");
             await Page.Personen_BirthDateInput().FillAsync("17-11-1952");
+
+            await Step("And clicks the search button");
+
             await Page.PersonenFirst_SearchButton().ClickAsync();
              
-            await Step("Verify navigation to the Persoonsinformatie page");
+            await Step("Then user is navigated to Persoonsinformatie page ");
 
             await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Persoonsinformatie" })).ToHaveTextAsync("Persoonsinformatie");
         
@@ -38,15 +45,22 @@ namespace Kiss.Bff.EndToEndTest.ContactMomentSearch
 
             await Step("Given the user is on the startpagina ");
 
+            await Page.GotoAsync("/");
+
+            await Step("When user starts a new contactmoment");
+
             await Page.CreateNewContactmomentAsync();
 
-            await Step("User fills Lastname and Birthday and perform search");
+            await Step("And user enters \"TestDB” in the field achternaam and \"11-12-1990” in the field geboortedatum ");
 
             await Page.Personen_LastNameInput().FillAsync("TestDB");
             await Page.Personen_BirthDateInput().FillAsync("11-12-1990");
+
+            await Step("And clicks the search button");
+
             await Page.PersonenFirst_SearchButton().ClickAsync();
 
-            await Step("The message is displayed as “Geen resultaten gevonden voor ’TestDB, 11-12-1990");
+            await Step("Then the message is displayed as “Geen resultaten gevonden voor ’TestDB, 11-12-1990’.");
            
             await  Expect(Page.GetByRole(AriaRole.Caption)).ToHaveTextAsync("Geen resultaten gevonden voor 'TestDB, 11-12-1990'.");
 
