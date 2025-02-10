413de33bb2e6c60941f5412333398e8a4653f932
Henok Solomon <henok@icatt.nl>
2025-02-10 21:42:07 +0300

    Enhance E2E tests for Afhandeling form
    Introduce new test methods and helper methods in AfhandelingFormScenarios and Locators classes. Add tests for error validation and successful submission of the Afhandeling form. Update locator methods to improve interaction with form elements

--- Kiss.Bff.EndToEndTest/AfhandelingForm/Helpers/Locators.cs
+++ Kiss.Bff.EndToEndTest/AfhandelingForm/Helpers/Locators.cs
@@ -27,5 +27,45 @@ namespace Kiss.Bff.EndToEndTest.AfhandelingForm.Helpers
         {
             return page.GetByRole(AriaRole.Textbox, new() { Name = "Notitie" });
         }
+ 
+
+        public static ILocator GetOpslaanButton(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Button, new() { Name = "Opslaan" });
+        }
+
+        public static ILocator GetAfhandelingForm(this IPage page)
+        {
+            return  page.Locator("form.afhandeling"); 
+        }
+
+        public static ILocator GetSpecificVraagField(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Textbox, new() { Name = "Specifieke vraag *" });
+        }
+
+        public static ILocator GetKanaalField(this IPage page)
+        {
+            return page.GetByLabel("Kanaal" );
+        }
+
+        public static ILocator GetAfhandelingField(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Combobox, new() { Name = "Afhandeling" });
+        }
+
+        public static ILocator GetAfdelingField(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Combobox, new() { Name = "Afdeling" });
+        }
+        public static ILocator GetAfdelingVoorField(this IPage page)
+        {
+            return page.Locator("input[type='search']");
+        }
+
+        public static ILocator GetAfhandelingSuccessToast(this IPage page)
+        {
+            return page.Locator("output[role='status'].confirm"); 
+        }
     }
 }
