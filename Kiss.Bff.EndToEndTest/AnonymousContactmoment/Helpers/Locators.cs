30f48039f7f35bd42769da5978495dccabdd5724
Henok Solomon <henok@icatt.nl>
2025-02-13 00:34:44 +0300

    Add new test class and update locators for contact moments
    Introduce `AnonymousContactmomentScenarios` test class with four
    methods to automate content search within contact moments. Update
    `Locators.cs` to include Playwright-based helper methods for
    locating elements like buttons, checkboxes, and search results.

---
+++ Kiss.Bff.EndToEndTest/AnonymousContactmoment/Helpers/Locators.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Playwright;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+
+namespace Kiss.Bff.EndToEndTest.AnonymousContactmomentBronnen.Helpers
+{
+    public static class Locators
+    {
+        public static ILocator GetNieuwContactmomentButton(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Button, new() { Name = "Nieuw contactmoment" });
+        }
+
+        public static ILocator GetContactmomentSearch(this IPage page)
+        {
+            return page.Locator(".contactmomentLoopt").GetByRole(AriaRole.Combobox);
+        }
+    
+
+        public static ILocator GetContactmomentSearchResults(this IPage page)
+        {
+            return page.Locator(".search-results.isExpanded").GetByRole(AriaRole.Navigation).First.GetByRole(AriaRole.Link);
+        }
+
+        public static ILocator GetSmoelenboekCheckbox(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Checkbox, new() { Name = "Smoelenboek" });
+        }
+
+        public static ILocator GetVACCheckbox(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Checkbox, new() { Name = "VAC" });
+        }
+
+        public static ILocator GetKennisbankCheckbox(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Checkbox, new() { Name = "Kennisbank" });
+        }
+  
+
+     
+    }
+   }
