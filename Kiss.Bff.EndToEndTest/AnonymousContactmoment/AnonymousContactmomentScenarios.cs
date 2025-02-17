8be4860d5086f883694ccdafc4f92d24129a79b6
Henok Solomon <henok@icatt.nl>
2025-02-17 20:48:32 +0300

    Enhance end-to-end tests with new scenarios and locators
    Updated AnonymousContactmomentScenarios.cs:
    - Added assertions to check for 10 items in the search pane with specific labels.
    - Introduced SearchForWebsiteInContactmoment test method.
    - Introduced FillAfdelingOnAfhandelingFormByViewingKennisartikel test method.
    
    Updated Locators.cs:
    - Added new locator methods: GetDeventerCheckbox, GetBijzonderhedenTab, GetArticleTitle, GetArticleHeading, GetPersonenAfrondenButton, GetAfhandelingForm, GetVraagField, GetAfdelingField.

--- Kiss.Bff.EndToEndTest/AnonymousContactmoment/AnonymousContactmomentScenarios.cs
+++ Kiss.Bff.EndToEndTest/AnonymousContactmoment/AnonymousContactmomentScenarios.cs
@@ -30,12 +30,12 @@ namespace Kiss.Bff.EndToEndTest.AnonymousContactmomentBronnen
 
             await Page.GetContactmomentSearch().PressAsync("Enter");
 
-            await Step("Then 10 items should appear in the Search pane");            
-           
+            await Step("Then 10 items should appear in the Search pane");
+
             await Expect(Page.GetContactmomentSearchResults()).ToHaveCountAsync(10);
 
             await Step("And each item has a label VAC or Kennisbank or Website in the first column");
-           
+
             await Task.WhenAll((await Page.GetContactmomentSearchResults().AllAsync()).Select(async item =>
             {
                 var firstColumn = item.Locator("span:nth-of-type(1)");
@@ -69,16 +69,16 @@ namespace Kiss.Bff.EndToEndTest.AnonymousContactmomentBronnen
             await Page.GetContactmomentSearch().PressAsync("Enter");
 
             await Step("Then 10 items should appear");
- 
+
             await Expect(Page.GetContactmomentSearchResults()).ToHaveCountAsync(10);
 
             await Step("And each item has a label Smoelenboek in the first column");
 
             await Task.WhenAll((await Page.GetContactmomentSearchResults().AllAsync()).Select(async item =>
-            { 
-            await Expect(item.Locator("span:nth-of-type(1)").Filter(new() { HasText = "Smoelenboek" })).ToBeVisibleAsync();
+            {
+                await Expect(item.Locator("span:nth-of-type(1)").Filter(new() { HasText = "Smoelenboek" })).ToBeVisibleAsync();
             }));
-            
+
         }
 
         [TestMethod("3. Search for VAC in Contactmoment")]
@@ -140,7 +140,7 @@ namespace Kiss.Bff.EndToEndTest.AnonymousContactmomentBronnen
             await Page.GetContactmomentSearch().PressAsync("Enter");
 
             await Step("Then 10 items should appear");
-             
+
             await Expect(Page.GetContactmomentSearchResults()).ToHaveCountAsync(10);
 
             await Step("And each item has a label Kennisbank in the first column");
@@ -151,6 +151,97 @@ namespace Kiss.Bff.EndToEndTest.AnonymousContactmomentBronnen
             }));
         }
 
-      
-       }
+        [TestMethod("5. Search for Website in Contactmoment")]
+        public async Task SearchForWebsiteInContactmoment()
+        {
+            await Step("Given the user is on the Startpagina");
+
+            await Page.GotoAsync("/");
+
+            await Step("When the user starts a new Contactmoment");
+
+            await Page.CreateNewContactmomentAsync();
+
+            await Step("And checks the box Deventer.nl in the Search pane");
+
+            await Page.GetDeventerCheckbox().CheckAsync();
+
+            await Step("And enters 'boom' in the search field in the Search pane");
+
+            await Page.GetContactmomentSearch().FillAsync("boom");
+
+            await Step("And presses Enter");
+
+            await Page.GetContactmomentSearch().PressAsync("Enter");
+
+            await Step("Then at least 1 item should appear");
+
+            await Expect(Page.GetContactmomentSearchResults()).ToBeVisibleAsync();
+            
+            Assert.IsTrue((await Page.GetContactmomentSearchResults().CountAsync()) > 0, "Expected at least 1 search result to appear.");
+
+            await Step("And the item has a label Website in the first column");
+
+            await Task.WhenAll((await Page.GetContactmomentSearchResults().AllAsync()).Select(async item =>
+            {
+                await Expect(item.Locator("span:nth-of-type(1)").Filter(new() { HasText = "Website" })).ToBeVisibleAsync();
+            }));
+        }
+
+        [TestMethod("6. Fill Afdeling on Afhandeling form by viewing Kennisartikel")]
+        public async Task FillAfdelingOnAfhandelingFormByViewingKennisartikel()
+        {
+            await Step("Given the user is on the Startpagina");
+
+            await Page.GotoAsync("/");
+
+            await Step("When the user starts a new Contactmoment");
+
+            await Page.CreateNewContactmomentAsync();
+
+            await Step("And enters 'andere achternaam gebruiken' in the search field in Search pane");
+
+            await Page.GetContactmomentSearch().FillAsync("andere achternaam gebruiken");
+
+            await Step("There should be 1 Kennisartikel in the list of results with the title 'Andere achternaam gebruiken'");
+
+            var item = Page.GetContactmomentSearchResults().Locator("span:nth-of-type(2)").Filter(new() { HasText = "Andere achternaam gebruiken" });
+           
+            await Expect(item).ToBeVisibleAsync();
+            await Expect(item).ToHaveCountAsync(1);
+             
+            await Step("When user clicks on the item 'Andere achternaam gebruiken'");
+
+            await item.ClickAsync();
+
+            await Step("Then the search pane should display the article with title 'Andere achternaam gebruiken' and heading 'Inleiding'");
+
+            await Expect(Page.GetArticleTitle()).ToBeVisibleAsync();
+            await Expect(Page.GetArticleHeading()).ToBeVisibleAsync();
+
+            await Step("And user clicks on 'Bijzonderheden' in the Search pane");
+
+            await Page.GetBijzonderhedenTab().ClickAsync();
+
+            await Step("And then clicks on Afronden in the Notes-Contactverzoek-Pane");
+
+            await Page.GetPersonenAfrondenButton().ClickAsync();
+
+            await Step("When Afhandeling form is displayed");
+
+            await Expect(Page.GetAfhandelingForm()).ToBeVisibleAsync();
+
+            await Step("Then the field 'Vraag' has value 'Andere achernaam gebruiken - Bijzonderheden'");
+                                                                                          
+            await Expect(Page.GetVraagField().Locator("option:checked")).ToHaveTextAsync("Andere achternaam gebruiken - Bijzonderheden");
+
+            await Step("And the dropdown list of the field Vraag has 8 items");
+   
+            await Expect(Page.GetVraagField().Locator("option")).ToHaveCountAsync(8);
+
+            await Step("And the field 'Afdeling' has value 'Publiekscontacten Burgertaken en gegevensbeheer'");
+
+            await Expect(Page.GetAfdelingField()).ToHaveValueAsync("Publiekscontacten Burgertaken en gegevensbeheer");
+        }
+    }
   }
--- Kiss.Bff.EndToEndTest/AnonymousContactmoment/Helpers/Locators.cs
+++ Kiss.Bff.EndToEndTest/AnonymousContactmoment/Helpers/Locators.cs
@@ -32,8 +32,45 @@ namespace Kiss.Bff.EndToEndTest.AnonymousContactmomentBronnen.Helpers
         {
             return page.GetByRole(AriaRole.Checkbox, new() { Name = "Kennisbank" });
         }
-  
 
-     
+        public static ILocator GetDeventerCheckbox(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Checkbox, new() { Name = "Deventer.nl" });
+        }
+        public static ILocator GetBijzonderhedenTab(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Link, new() { Name = "Bijzonderheden" });
+        }
+        public static ILocator GetArticleTitle(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Article).GetByRole(AriaRole.Heading, new() { Name = "Andere achternaam gebruiken" });
+          }
+
+        public static ILocator GetArticleHeading(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Article).GetByRole(AriaRole.Heading, new() { Name = "Inleiding" });
+        }
+
+       
+
+        public static ILocator GetPersonenAfrondenButton(this IPage page)
+        {
+            return page.GetByRole(AriaRole.Button, new() { Name = "Afronden" });
+        }
+
+        public static ILocator GetAfhandelingForm(this IPage page)
+        {
+            return page.Locator("form.afhandeling");
+        }
+
+        public static ILocator GetVraagField(this IPage page)
+        {
+            return page.GetByLabel("Vraag", new() { Exact = true });
+        }
+        public static ILocator GetAfdelingField(this IPage page)
+        {
+            return page.Locator("input[type=\"search\"]");
+        } 
+       
     }
    }
