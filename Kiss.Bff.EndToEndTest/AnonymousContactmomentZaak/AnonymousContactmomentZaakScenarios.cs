4a09ca20c557ec8c800b6c10a61e275bee12f181
Henok Solomon <henok@icatt.nl>
2025-02-19 12:17:13 +0300

    Add tests for AnonymousContactmomentZaak feature
    Introduce AnonymousContactmomentZaakScenarios test class with four test methods to verify search, registration, and viewing of Contactmoment for a Zaak. Update Locators.cs with helper methods for web element interactions in tests.

---
+++ Kiss.Bff.EndToEndTest/AnonymousContactmomentZaak/AnonymousContactmomentZaakScenarios.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Kiss.Bff.EndToEndTest.AnonymousContactmomentZaak.Helpers;
+using Kiss.Bff.EndToEndTest.ContactMomentSearch.Helpers;
+
+namespace Kiss.Bff.EndToEndTest.AnonymousContactmomentZaak
+{
+    [TestClass]
+    public class AnonymousContactmomentZaakScenarios : KissPlaywrightTest
+    {
+        [TestMethod("1. Search for Zaak in Contactmoment")]
+        public async Task SearchForZaakInContactmoment()
+        {
+            await Step("Given the user has started a new Contactmoment");
+
+            await Page.CreateNewContactmomentAsync();
+
+            await Step("When the user clicks on 'Zaken' in the menu");
+
+            await Page.GetZakenMenu().ClickAsync();
+
+            await Step("And enters 'ZAAK-2023-001' in the search field");
+
+            await Page.GetZaakSearchField().FillAsync("ZAAK-2023-001");
+
+            await Step("And clicks zoeken (magnifying glass-icon)");
+
+            await Page.GetZaakSearchButton().ClickAsync();
+
+            await Step("Then the user will navigate to the screen 'Zaak ZAAK-2023-001'");
+
+            await Expect(Page.GetZaakScreen("ZAAK-2023-001")).ToBeVisibleAsync();
+        }
+
+        [TestMethod("2. Register Contactmoment bij Zaak - I")]
+        public async Task RegisterContactmomentBijZaakI()
+        {
+            await Step("Precondition: The scenario 'Search for Zaak in Contactmoment' has been executed");
+
+            await SearchForZaakInContactmoment();
+
+            await Step("Given the user is on the 'Zaak ZAAK-2023-001' screen");
+
+            await Expect(Page.GetZaakScreen("ZAAK-2023-001")).ToBeVisibleAsync();
+
+            await Step("When the user clicks Afronden in the Notes-Contactverzoek-Pane");
+
+            await Page.GetPersonenAfrondenButton().ClickAsync();
+
+            await Step("And the Afhandeling form is displayed");
+
+            await Expect(Page.GetAfhandelingForm()).ToBeVisibleAsync();
+
+            await Step("Then the heading 'Gerelateerde zaak' appears under 'Vraag 1'");
+
+            await Expect(Page.GetGerelateerdeZaakHeading()).ToBeVisibleAsync();
+
+            await Step("And the item 'ZAAK-2023-001' is listed");
+
+            await Expect(Page.GetZaakItem()).ToBeVisibleAsync();
+
+            await Step("And a checked checkbox is visible");
+
+            await Expect(Page.GetZaakCheckbox()).ToBeCheckedAsync();
+        }
+
+        [TestMethod("3. Register Contactmoment bij Zaak - II")]
+        public async Task RegisterContactmomentBijZaakII()
+        {
+            await Step("Precondition: Scenario: Register Contactmoment bij Zaak – I is executed");
+
+            await RegisterContactmomentBijZaakI();
+
+            await Step("Given the user is on the Afhandeling-form");
+
+            await Expect(Page.GetAfhandelingForm()).ToBeVisibleAsync();
+
+            await Step("When user enters value 'Contactmoment bij ZAAK-2023-001' in field Specifieke vraag");
+
+            await Page.GetSpecificVraagField().FillAsync("Contactmoment bij ZAAK-2023-001");
+
+            await Step("And user enters 'Live chat' in field Kanaal");
+
+            await Page.GetKanaalField().ClickAsync();
+
+            await Page.GetKanaalField().SelectOptionAsync(new[] { new SelectOptionValue { Label = "Live Chat" } });
+
+            await Step("And value 'Zelfstandig afgehandeld' in field Afhandeling");
+
+            await Page.GetAfhandelingField().SelectOptionAsync(new[] { "Zelfstandig afgehandeld" });
+
+            await Step("And selects value 'Parkeren' in field Afdeling");
+
+            await Page.GetAfdelingField().ClickAsync();
+            await Page.GetByText("Parkeren").ClickAsync();
+
+            await Step("And clicks on Opslaan button");
+
+            await Page.GetOpslaanButton().ClickAsync();
+
+            await Step("Then message as 'Het contactmoment is opgeslagen' is displayed on the Startpagina");
+
+            await Expect(Page.GetSuccessMessage()).ToHaveTextAsync("Het contactmoment is opgeslagen");
+        }
+
+        [TestMethod("4. View Contactmoment bij Zaak")]
+        public async Task ViewContactmomentBijZaak()
+        {
+            await Step("Precondition: Scenario: Register Contactmoment bij Zaak – II is executed");
+
+            await RegisterContactmomentBijZaakII();
+
+            await Step("Given the user is on the Startpagina");
+
+            await Page.GotoAsync("/");
+
+            await Step("When the user starts a new Contactmoment");
+
+            await Page.CreateNewContactmomentAsync();
+
+            await Step("And clicks on 'Zaken' in the menu");
+
+            await Page.GetZakenMenu().ClickAsync();
+
+            await Step("And enters 'ZAAK-2023-001' in the search field");
+
+            await Page.GetZaakSearchField().FillAsync("ZAAK-2023-001");
+
+            await Step("And clicks zoeken");
+
+            await Page.GetZaakSearchButton().ClickAsync();
+
+            await Step("And navigates to the screen 'Zaak ZAAK-2023-001'");
+
+            await Expect(Page.GetZaakScreen("ZAAK-2023-001")).ToBeVisibleAsync();
+
+            await Step("And clicks on tab Contactmomenten");
+
+            await Page.GetContactmomentenTab().ClickAsync();
+
+            await Step("Then the first Contactmoment should contain the details as entered in Scenario: Register Contactmoment bij Zaak – II");
+
+            var firstContactMomentSummary = Page.GetFirstContactMomentSummary();
+            await Expect(firstContactMomentSummary.GetGespreksresultaatHeader()).ToHaveTextAsync("Zelfstandig afgehandeld");
+            await Expect(firstContactMomentSummary.GetAfdelingHeader()).ToHaveTextAsync("Parkeren");
+
+            await Page.Locator("ul.overview > li:nth-child(2) details").EvaluateAsync("el => el.open = true");
+
+            var detailsList = Page.GetDetailsList();
+            await Expect(detailsList.GetZaaknummer()).ToHaveTextAsync("ZAAK-2023-001");
+            await Expect(detailsList.GetSpecifiekeVraag()).ToHaveTextAsync("Contactmoment bij ZAAK-2023-001");
+        }
+    }
+    
+}
---
+++ Kiss.Bff.EndToEndTest/AnonymousContactmomentZaak/Helpers/Locators.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiss.Bff.EndToEndTest.AnonymousContactmomentZaak.Helpers
+{
+   
+        public static class Locators
+        {
+            public static ILocator GetNieuwContactmomentButton(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Button, new() { Name = "Nieuw contactmoment" });
+            }
+
+            public static ILocator GetContactmomentSearch(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Combobox, new() { Name = "Zoekterm" });
+            }
+
+            public static ILocator GetContactmomentSearchResults(this IPage page)
+            {
+                return page.Locator(".search-results.isExpanded ul li");
+            }
+
+            public static ILocator GetSmoelenboekCheckbox(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Checkbox, new() { Name = "Smoelenboek" });
+            }
+
+            public static ILocator GetVACCheckbox(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Checkbox, new() { Name = "VAC" });
+            }
+
+            public static ILocator GetKennisbankCheckbox(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Checkbox, new() { Name = "Kennisbank" });
+            }
+
+            public static ILocator GetDeventerCheckbox(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Checkbox, new() { Name = "Deventer.nl" });
+            }
+
+            public static ILocator GetZakenMenu(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Link, new() { Name = "Zaken" });
+            }
+
+            public static ILocator GetZaakSearchField(this IPage page)
+            {
+                return page.GetByTitle("ZAAK-1");
+            }
+
+            public static ILocator GetZaakSearchButton(this IPage page)
+            {
+                return page.GetByTitle("Zoeken");
+            }
+
+            public static ILocator GetZaakScreen(this IPage page, string zaakId)
+            {
+                return page.GetByRole(AriaRole.Heading, new() { Name = $"Zaak {zaakId}" });
+            }
+
+            public static ILocator GetPersonenAfrondenButton(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Button, new() { Name = "Afronden" });
+            }
+
+            public static ILocator GetAfhandelingForm(this IPage page)
+            {
+            return page.Locator("form.afhandeling");
+        }
+
+            public static ILocator GetGerelateerdeZaakHeading(this IPage page)
+            {
+            var vraagHeading = page.GetByRole(AriaRole.Heading, new() { Name = "Vraag 1" });
+           
+            return vraagHeading.Locator("..").GetByRole(AriaRole.Heading, new() { Name = "Gerelateerde zaak" });
+
+            }
+
+            public static ILocator GetZaakItem(this IPage page)
+            {
+                return page.GetByText("ZAAK-2023-001");
+            }
+
+            public static ILocator GetZaakCheckbox(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Checkbox, new() { Name = "ZAAK-2023-001" });
+            ;
+        }
+
+            public static ILocator GetSpecificVraagField(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Textbox, new() { Name = "Specifieke vraag" });
+            }
+
+            public static ILocator GetAfhandelingField(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Combobox, new() { Name = "Afhandeling" });
+            }
+
+            public static ILocator GetAfdelingField(this IPage page)
+            {
+                return page.Locator("input[type='search']");
+            }
+
+            public static ILocator GetKanaalField(this IPage page)
+            {
+                return page.GetByLabel("Kanaal");
+            }
+        public static ILocator GetOpslaanButton(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Button, new() { Name = "Opslaan" });
+            }
+
+            public static ILocator GetSuccessMessage(this IPage page)
+            {
+                  return page.Locator("output[role='status'].confirm");
+            }
+
+            public static ILocator GetContactmomentenTab(this IPage page)
+            {
+                return page.GetByRole(AriaRole.Tab, new() { Name = "Contactmomenten" });
+            }
+
+            public static ILocator GetFirstContactmoment(this IPage page)
+            {
+            return page.Locator("ul.overview > li:nth-child(2) summary");
+        }
+        public static ILocator GetFirstContactMomentSummary(this IPage page) => page.GetFirstContactmoment();
+        public static ILocator GetMedewerkerHeader(this ILocator locator) => locator.Locator("span[aria-describedby='medewerker-header']");
+        public static ILocator GetGespreksresultaatHeader(this ILocator locator) => locator.Locator("span[aria-describedby='gespreksresultaat-header']");
+        public static ILocator GetAfdelingHeader(this ILocator locator) => locator.Locator("span[aria-describedby='afdeling-header']");
+        public static ILocator GetDetailsList(this IPage page) => page.Locator("ul.overview > li:nth-child(2) dl");
+        public static ILocator GetZaaknummer(this ILocator locator) => locator.Locator("dd:nth-of-type(2)");
+        public static ILocator GetSpecifiekeVraag(this ILocator locator) => locator.Locator("dd:nth-of-type(3)");
+
+
+    }
+}
+
+
